069fc987f1814cc3ddbb2c97b5c41eb51ef07d64
Chris Schaefer <chris@dtzq.com>
2016-12-15 20:12:59 -0500

    Update badge authorization service to log to activity_log - #16

--- src/main/java/io/srqsoftware/DefaultBadgeAuthorizationService.java
+++ src/main/java/io/srqsoftware/DefaultBadgeAuthorizationService.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.apache.log4j.Logger;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 @Component
 public class DefaultBadgeAuthorizationService implements BadgeAuthorizationService {
@@ -17,12 +20,16 @@ public class DefaultBadgeAuthorizationService implements BadgeAuthorizationServi
 	}
 
 	@Override
+	@Transactional
 	public int getAuthorizationStatus(String badgeId, String deviceId) {
 		try {
 			String query = "select count(badge_id) from badges where badge_id=?";
 			int activeBadges = jdbcTemplate.queryForObject(query, new Object[]{badgeId}, Integer.class);
 
 			LOGGER.info("Badge id: " + badgeId + " for device id:" + deviceId + " - " + (activeBadges > 0 ? "authorized" : "unauthorized"));
+
+			String logQuery = "insert into activity_log (time, badge_id, device_id, status) values (?,?,?,?)";
+			jdbcTemplate.update(logQuery, new Date(), badgeId, deviceId, activeBadges);
 
 			return activeBadges > 0 ? AUTHORIZED : UNAUTHORIZED;
 		} catch (Exception e) {
